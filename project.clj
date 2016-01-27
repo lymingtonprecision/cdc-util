87b52b4829ea74a912ace29835e5a5cbae96aecd
Adam Harper <github@adam-harper.com>
2016-01-27 10:21:01 +0000

    Version 1.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lymingtonprecision/cdc-util "1.2.2"
+(defproject lymingtonprecision/cdc-util "1.3.0-SNAPSHOT"
   :description "LPE Change Data Capture utility library"
   :url "https://github.com/lymingtonprecision/cdc-util"
   :license {:name "Eclipse Public License"
